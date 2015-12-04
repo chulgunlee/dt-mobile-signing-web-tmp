390180c025866bc4a50837a2ad8bf8f040bc3770
Charlee Li <charlee@envisageny.com>
2015-12-04 01:06:02 +0000

    use six.iteritems() to iterate dictionary

--- signingroom/lib/dt_logging.py
+++ signingroom/lib/dt_logging.py
@@ -1,4 +1,5 @@
 import logging
+import six
 from django.conf import settings
 
 class SigningWebLoggingFilter(logging.Filter):
@@ -8,7 +9,7 @@ class SigningWebLoggingFilter(logging.Filter):
     def filter(self, record):
         "Inject LOGGING_EXTRA_DATA into log record"
 
-        for k, v in settings.LOGGING_EXTRA_DATA.iteritems():
+        for k, v in six.iteritems(settings.LOGGING_EXTRA_DATA):
             if not hasattr(record, k):
                 setattr(record, k, v)
         return True
