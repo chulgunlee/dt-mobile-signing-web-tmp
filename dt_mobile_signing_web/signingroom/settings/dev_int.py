7f968345b738393e615ef21a4002888a114db270
tommy.han <tommy.han@dealertrack.com>
2016-01-06 17:30:50 -0500

    add URI for devint settings

--- dt_mobile_signing_web/signingroom/settings/dev_int.py
+++ dt_mobile_signing_web/signingroom/settings/dev_int.py
@@ -1,3 +1,14 @@
-from doc_center_api.settings.base import *  # noqa
+from .base import *  # noqa
 
-# add your settings here
+
+DEBUG = True
+VERIFY_WS_CERT = False
+
+
+# mobile server api uri
+#MOBILE_SERVER_URI = 'https://ws.di.dealertrack.com/api/mobile'
+MOBILE_SERVER_URI = 'http://localhost:6110/api/mobile'
+DOCCENTER_URI = 'http://localhost:6112/api/doccenter'
+
+# doc center api uri
+DOCCENTER_SERVER_URI = ''
