266b8624a62266f2e2e39f6fb6f0f02532416e4e
Charlee Li <charlee@envisageny.com>
2016-02-10 22:10:03 +0000

    added qa config

---
+++ dt_mobile_signing_web/signingroom/settings/qa.py
@@ -0,0 +1,9 @@
+from .base import *             # noqa
+
+DEBUG = True
+VERIFY_WS_CERT = False
+
+MOBILE_SERVER_URI = 'http://localhost:6110/api/mobile'
+DOCCENTER_URI = 'http://localhost:6112/api/doccenter'
+
+URI_ROOT = '/mobile-web/'           # temporarily set for dev_int
