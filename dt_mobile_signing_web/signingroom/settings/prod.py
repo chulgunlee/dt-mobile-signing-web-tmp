2b4843bbdb2640199e16e86ee43bb6dade348d34
Charlee Li <charlee@envisageny.com>
2016-02-10 22:12:16 +0000

    added prod config

---
+++ dt_mobile_signing_web/signingroom/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *             # noqa
+
+DEBUG = False
+VERIFY_WS_CERT = False
+
+MOBILE_SERVER_URI = 'http://localhost:6110/api/mobile'
+DOCCENTER_URI = 'http://localhost:6112/api/doccenter'
+
+URI_ROOT = '/'           # temporarily set for dev_int
